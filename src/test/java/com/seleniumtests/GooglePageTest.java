2c52384747db4c78d9fe53e5b7edd12e7c695ba1
Balaji-Developers <104586512+Balaji-Developers@users.noreply.github.com>
2026-02-13 15:15:30 +0530

    Added files to git

---
+++ src/test/java/com/seleniumtests/GooglePageTest.java
@@ -0,0 +1,12 @@
+package com.seleniumtests;
+
+public class GooglePageTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is my first code");
+
+	}
+
+}
